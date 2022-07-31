5e9c55e63cefbf61d99640c1deda187678694d52
Joao Marcos <joomarcos.belanga98@gmail.com>
2022-07-30 21:50:47 -0300

    test: :test_tube: Add tests for the Sign In Button

--- src/components/SignInButton/index.test.tsx
+++ src/components/SignInButton/index.test.tsx
@@ -1,17 +1,39 @@
 import { screen } from "@testing-library/react";
 import { renderWithTheme } from "utils/tests/renderWithTheme";
+import userEvent from '@testing-library/user-event'
 
 import { SignInButton } from ".";
 
-describe("<SignInButton/>", () => {
-    it("should render the heading", () => {
-        const { container } = renderWithTheme(<SignInButton />);
-        renderWithTheme
+const mock = jest.fn()
 
+describe("<SignInButton/>", () => {
+    it("should be render correctly", () => {
+        const { container } = renderWithTheme(<SignInButton onSignIn={mock} />);
+        
         expect(
-            screen.getByRole("heading", { name: /SignInButton/i })
+            screen.getByRole("button", { name: /Sign In with Google/i })
         ).toBeInTheDocument();
 
         expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it("should be when user click dispath onSignIn", () => {
+        renderWithTheme(<SignInButton onSignIn={mock} />);
+
+        userEvent.click(screen.getByRole("button", { name: /Sign In with Google/i }))
+
+        expect(mock).toHaveBeenCalledTimes(1);
+    });
+
+    it("should be have accessibility", () => {
+        renderWithTheme(<SignInButton onSignIn={mock} />);
+
+        expect(document.body).toHaveFocus();
+
+        userEvent.tab()
+
+        expect(
+            screen.getByRole("button", { name: /Sign In with Google/i })
+        ).toHaveFocus();
     });
 });
