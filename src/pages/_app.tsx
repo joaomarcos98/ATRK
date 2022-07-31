459ed1fc3b5dca2196be3eb3f3ce2fbe3d9f0864
Joao Marcos <joomarcos.belanga98@gmail.com>
2022-07-30 21:37:40 -0300

    style: :lipstick: Adding theme in the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,24 @@
-import { AppProps } from "next/app";
-import Head from "next/head";
+import Head from "next/head"
+import { AppProps } from "next/app"
+import { ThemeProvider } from "styled-components"
 
-import { GlobalStyles } from "styles/global";
+import theme from "styles/theme"
+
+import { GlobalStyles } from "styles/global"
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
+        <ThemeProvider theme={theme}>
             <Head>
-                <title>React Avançado - Boilerplate</title>
+                <title>Espaço zen</title>
                 <link rel="shortcut icon" href="/img/icon-512.png" />
                 <link rel="apple-touch-icon" href="/img/icon-512.png" />
                 <link rel="manifest" href="/manifest.json" />
             </Head>
             <GlobalStyles />
             <Component {...pageProps} />
+        </ThemeProvider>
         </>
-    );
+    )
 }
