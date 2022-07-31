2976d8b19bab0c1f727e96a63d2f80966772d625
Joao Marcos <joomarcos.belanga98@gmail.com>
2022-07-30 22:11:06 -0300

    refactor: :wrench: Add configs for the storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,32 @@
-import { GlobalStyles } from "../src/styles/global";
+import { RouterContext } from 'next/dist/shared/lib/router-context'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyles } from '../src/styles/global'
+import theme from '../src/styles/theme'
+
+export const parameters = {
+    nextRouter: {
+        Provider: RouterContext.Provider
+    },
+    backgrounds: {
+        default: 'light',
+        values: [
+            {
+                name: 'light',
+                value: theme.colors.white
+            },
+            {
+                name: 'dark',
+                value: theme.colors.black
+            },
+        ]
+    }
+}
 
 export const decorators = [
-    (Story) => (
-        <>
-            <GlobalStyles />
+    Story => (
+        <ThemeProvider theme={theme}>
+            <GlobalStyles removeBg />
             <Story />
-        </>
+        </ThemeProvider>
     )
 ]
